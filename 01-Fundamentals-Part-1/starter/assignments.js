80427caaa2b1df53c3413c35baac2d2eed1e4ef2
byjohnnay <78664835+conquerthecrowns@users.noreply.github.com>
2023-06-08 01:26:23 -0700

    lesson2

--- 01-Fundamentals-Part-1/starter/assignments.js
+++
@@ -1,5 +0,0 @@
-let country = 'Canada';
-let continent = 'BC';
-let population = '2mil';
-
-console.log(country, continent, population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,20 @@
-let js = 'amazing';
-console.log(40+8+23-10);
 
-let firstName = 'Andrew';
-console.log(firstName);
+// let testing = true;
+// console.log(typeof testing)
+// testing = 'Naw';
+// console.log(typeof testing)
+
+// let year; 
+// const birthYear = 1991;
+
+const firstName = 'Johnny';
+const job = 'Sr Developer';
+const birthYear = '1997';
+const year = 2023;
+
+const john = "I'm "  + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!'
+
+console.log(john);
+
+const johnNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(johnNew)
